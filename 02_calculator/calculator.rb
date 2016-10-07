d3304a72009b5b3d9ba08bb846b71cd6d179a326
Lorna <bikergirl1515@hotmail.com>
2016-10-07 13:56:25 +0000

    02_calculator finished

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,27 @@
-#write your code here
+def add(a, b)
+  a + b
+end
+
+def subtract(a, b)
+  a - b
+end
+
+def sum(array)
+  array.inject(0){|sum,x| sum + x }
+end
+
+def multiply(array)
+  array.inject(1){|mul,x| mul * x }
+end
+
+def power(a, b)
+  a ** b
+end
+
+def factorial(n)
+  total = 1
+  (1..n).each do |n|
+    total *= n   
+  end
+  total
+end
